4dcd350257461d094ec3602b92e7bd9edb73b44b
TheRayOfSeasons <sison.ray@ciit.edu.ph>
2018-10-03 12:46:03 +0800

    DatabaseHandler with dates

--- src/Java/DatabaseHandler.java
+++ src/Java/DatabaseHandler.java
@@ -1,11 +1,32 @@
 package Java;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 
 public class DatabaseHandler
 {
     private static Connection conn = null;
     private static Statement stmt = null;
+
+    //Tables
+    private static String
+        CashReceiptA = "CASHRECEIPT_A",
+        CashOnHandA = "CASHONHAND_A",
+        LoansReceivableA = "LOANSRECEIVABLE_A",
+        SundryAccountsA = "SUNDRYACCOUNTS_A";
+
+    //Columns
+    private static String
+        uuid = "UUID",
+        date = "\"DATE\"",
+        particular = "PARTICULAR",
+        referenceNo = "REFERENCE_NO",
+        debit = "DEBIT",
+        credit = "CREDIT",
+        typeOfLoanNo = "TYPE_OF_LOAN_NO",
+        accountNo = "ACCOUNT_NO";
 
     public static void InitiateDatabase ()
     {
@@ -15,31 +36,35 @@ public class DatabaseHandler
             System.out.println("Opened database successfully");
             stmt = conn.createStatement();
             String sql =
-                    "CREATE TABLE IF NOT EXISTS CASHRECEIPT_A " +
-                    "(ID INT PRIMARY KEY     NOT NULL, " +
-                    " \"DATE\"        DATE   NOT NULL, " +
-                    " PARTICULAR      TEXT   NOT NULL, " +
-                    " REFERENCE_NO    TEXT   NOT NULL);" +
+                    "CREATE TABLE IF NOT EXISTS " + CashReceiptA +
+                    " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    uuid + "          TEXT   NOT NULL, " +
+                    date + "          DATE   NOT NULL, " +
+                    particular + "    TEXT   NOT NULL, " +
+                    referenceNo + "   TEXT   NOT NULL);" +
 
-                    "CREATE TABLE IF NOT EXISTS CASHONHAND_A" +
-                    "(ID INT PRIMARY KEY     NOT NULL, " +
-                    " REFERENCE_NO    TEXT   NOT NULL, " +
-                    " DEBIT           INTEGER, " +
-                    " CREDIT          INTEGER);" +
+                    "CREATE TABLE IF NOT EXISTS " + CashOnHandA +
+                    " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    uuid + "          TEXT   NOT NULL, " +
+                    referenceNo + "   TEXT   NOT NULL, " +
+                    debit + "         INTEGER, " +
+                    credit + "        INTEGER);" +
 
-                    "CREATE TABLE IF NOT EXISTS LOANSRECEIVABLE_A" +
-                    "(ID INT PRIMARY KEY     NOT NULL, " +
-                    " REFERENCE_NO    TEXT   NOT NULL, " +
-                    " TYPE_OF_LOAN_NO TEXT, " +
-                    " ACCOUNT_NO      TEXT, " +
-                    " CREDIT          INTEGER);" +
+                    "CREATE TABLE IF NOT EXISTS " + LoansReceivableA +
+                    " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    uuid + "          TEXT   NOT NULL, " +
+                    referenceNo + "   TEXT   NOT NULL, " +
+                    typeOfLoanNo + "  TEXT, " +
+                    accountNo + "     TEXT, " +
+                    credit + "        INTEGER);" +
 
-                    "CREATE TABLE IF NOT EXISTS SUNDRYACCOUNTS_A" +
-                    "(ID INT PRIMARY KEY     NOT NULL, " +
-                    " REFERENCE_NO    TEXT   NOT NULL, " +
-                    " ACCOUNT_NO      TEXT, " +
-                    " DEBIT           INTEGER, " +
-                    " CREDIT          INTEGER);";
+                    "CREATE TABLE IF NOT EXISTS " + SundryAccountsA +
+                    " (ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    uuid + "          TEXT   NOT NULL, " +
+                    referenceNo + "   TEXT   NOT NULL, " +
+                    accountNo + "     TEXT, " +
+                    debit + "         INTEGER, " +
+                    credit + "        INTEGER);";
 
 //            "CREATE TABLE IF NOT EXISTS OFFICIAL_RECEIPT " +
 //                    "(ID INT PRIMARY KEY     NOT NULL, " +
@@ -59,20 +84,32 @@ public class DatabaseHandler
         }
         System.out.println("Table created successfully");
 
+        AddDataCashReceiptA();
 //        AddData();
     }
-
-    public static void AddData ()
+    public static void AddDataCashReceiptA ()
     {
         try {
             stmt = conn.createStatement();
-            String sql = "INSERT INTO TESTTABLE " +
-                    "VALUES (\"1\",\"Ray\", 20, \"wer\", \"10000\")";
+            String sql = "INSERT INTO " + CashReceiptA +
+                    " (" + uuid + ", " + date + ", " + particular + ", " + referenceNo + ")" +
+                    " VALUES (" +
+                    "\"" + UUID.randomUUID().toString() + "\"" + ", " +
+                    "\"" + GetCurrentDate() + "\"" + ", " +
+                    "\"Particular Sample\", " +
+                    "\"10340\");";
             stmt.execute(sql);
             stmt.close();
             conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Date GetCurrentDate()
+    {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("YYYY-MM-DD");
+        LocalDate localDate = LocalDate.now();
+        return Date.valueOf(localDate);
     }
 }
